7f02417963c11ccd39bb9a5fa8bc541f51ec4d2d
Shihab-10 <ss7801491@gmail.com>
2024-10-19 23:13:53 +0600

    Add files via upload

---
+++ 9.2.c
@@ -0,0 +1,14 @@
+// #include<stdio.h>
+// int main(){
+//   int n;
+//   printf("enter the number :");
+//   scanf("%d",&n);
+//   int sum =0;
+//   while (n!=0){
+//     sum = sum + (n%10);
+//       n = n/10;
+//   }
+//   printf ("the sum of the given digit is %d",sum);
+//   return 0;
+  
+// }
---
+++ 9.4.c
@@ -0,0 +1,17 @@
+// #include<stdio.h>
+// int main(){
+//   int n;
+//   printf("enter the number :");
+//     scanf("%d",&n);
+//   int br =0;
+//   int pr =1;
+//   int rd;
+//   while (n!=0){
+//     rd = n%2;
+//     n = n/2;
+//     br = br + (pr *rd);
+//     pr=pr*10;
+    
+//   }
+//   printf ("the binary number is %d",br);
+// }
