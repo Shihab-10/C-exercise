34e8c0e122d9cfec4e461ce4cdb7e9a862d117c9
Shihab-10 <ss7801491@gmail.com>
2024-10-19 23:17:08 +0600

    Update 9.24.c

--- 9.24.c
+++ 9.24.c
@@ -9,7 +9,7 @@ int main() {
     printf("Prime numbers from 1 to %d are:\n", n);
 
     for (int num = 2; num <= n; num++) {
-        int a = 0;  // Initialize for each number
+        int a = 0;
 
         for (int i = 2; i < num; i++) {
             if (num % i == 0) {
