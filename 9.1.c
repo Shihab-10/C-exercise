304a83a55d1d4cfc745ec28a289cdaba49920711
Shihab-10 <ss7801491@gmail.com>
2024-10-20 19:03:47 +0600

    Update 9.1.c

--- 9.1.c
+++ 9.1.c
@@ -1,13 +1,13 @@
-// #include<stdio.h>
-// int main(){
-//   int n;
-//   printf("enter the number :");
-//   scanf("%d",&n);
-//   int  p =1;
-//   for (int i=1;i<=n;++i){
-//     p=p*i;
-//   }
-//   printf ("the factorial of the number is %d",p);
+#include<stdio.h>
+int main(){
+  int n;
+  printf("enter the number :");
+  scanf("%d",&n);
+  int  p =1;
+  for (int i=1;i<=n;++i){
+    p=p*i;
+  }
+  printf ("the factorial of the number is %d",p);
   
-//   return 0;
-// }
+  return 0;
+}
