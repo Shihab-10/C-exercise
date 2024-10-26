45f184607f11a9f77b2e6188338161dbecc91a95
Shihab-10 <ss7801491@gmail.com>
2024-10-27 02:24:19 +0600

    9.26.c

--- 9.26.c
+++ 9.26.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-
 int main() {
     int n, a, count = 0;
     printf("Enter the value of n: ");
     scanf("%d", &n);
     for (int i=2;i<=n;i++) {
-        a=0;
-        for (int j=2; j<=i / 2; j++) {
-            if (i%j == 0) {
-                a = 1;
-                break;
-            }
-        }
-        if (a==0) {
+     a=0;
+    for (int j=2; j<=i / 2; j++) {
+     if (i%j == 0) {
+    a = 1;
+     break;
+}
+}
+    if (a==0) {
             count++; 
         }
     }
